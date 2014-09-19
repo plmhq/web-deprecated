2821a50d8c0b3f695f286b215649f5e6296f4b2f
srph <seraphipod@gmail.com>
2014-09-20 06:51:08 +0800

    (moved) fonts to html. (added) typo utility.

--- app/views/index.php
+++ app/views/index.php
@@ -4,13 +4,19 @@
 	<title ng-bind="'Pamantasan ng Lungsod ng Maynila - ' + $state.current.data.title"> Pamantasan ng Lungsod ng Maynila </title>
 	<meta charset="utf-8">
 
-	<!-- Stylesheets -->
+	<!-- fonts -->
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic">
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Merriweather:400,700italic,400italic,300,900,700,900italic,300italic">
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat:400,700">
+
+	<!-- stylesheets -->
 	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 	<link href="/src/vendor/bootstrap/dist/css/bootstrap.min.css" type="text/css" rel="stylesheet">
 	<link href="/src/vendor/slick-carousel/slick/slick.css" type="text/css" rel="stylesheet">
 	<link href="/src/vendor/ngprogress-lite/ngprogress-lite.css" type="text/css" rel="stylesheet">
 	<link href="/assets/css/main.min.css" type="text/css" rel="stylesheet">
-	<!-- Favicon -->
+	
+	<!-- favicon -->
 	<link type="image/png" rel="shortcut icon" href="/assets/img/favicon.png">
 
 	<base href="/">
